5e9e14139cf740f03a294b40f5de6eba43b41db5
sethgoldin <sethgoldin@gmail.com>
2020-03-04 23:59:07 -0500

    Create receive.sh

---
+++ receive.sh
@@ -0,0 +1,17 @@
+# prompt user for the name of the s3 bucket
+
+# create a variable for the name of the s3 bucket that the user specified
+
+# prompt user for the exact path into where they want the data to go
+
+# create a variable for the exact path for where the data should go
+
+# s3 sync from s3 bucket to the local system
+
+# find the .mhl file that exists in the destination
+#			how do you check for the existence of a file in a directory?
+#		if no .mhl exists, throw an error
+#		if mhl verify throws an error, pass that through: display it to the user
+#			how do you pass an error code through and display it?
+#		if mhl is sucessful with exit code 0, display a message that all the data has been downloaded and verified
+#			how do you pass through a successful status?
