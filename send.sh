c5829c77ea7d9e3a9eabf20a8060688d5937e181
sethgoldin <sethgoldin@gmail.com>
2020-03-04 23:43:08 -0500

    Create send.sh

---
+++ send.sh
@@ -0,0 +1,19 @@
+# prompt user for path of folder
+#		without trailing space or no?
+
+# make a variable to represent path of folder on local system
+
+# prompt user for name of s3 bucket
+
+# make a variable to represent the name of the s3 bucket
+
+# mhl seal the source folder with xxHash
+#		what's the best practice for a binary that requires to be in the same directory?
+
+# mb
+
+# sync into the bucket
+
+# passthrough the info that the aws s3 cli would be displaying?
+
+# let user know that the sealed upload to s3 has completed
