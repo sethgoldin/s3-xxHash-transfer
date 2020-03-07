ec06045439c1ba1a4ab1a9896ccce0b24d3e9ab1
Seth Goldin <sethgoldin@gmail.com>
2020-03-07 17:48:16 -0500

    Good working prototype of Linux send.sh

--- send.sh
+++ send.sh
@@ -1,19 +1,33 @@
-# prompt user for path of folder
-#		without trailing space or no?
+#!/bin/bash
 
-# make a variable to represent path of folder on local system
+# Here's where the user is going to specify what source folder should be uploaded into S3
 
-# prompt user for name of s3 bucket
+read -e -p "From what directory do you want to upload data into S3? Please enter the absolute path and escape any spaces if necessary: " sourceLocalDirectory
 
-# make a variable to represent the name of the s3 bucket
+# Now $sourceLocalDirectory will work as the variable for the source folder from the local system that will be ingested into S3
 
-# mhl seal the source folder with xxHash
-#		what's the best practice for a binary that requires to be in the same directory?
+# Let's prompt the user for the name of the S3 bucket
 
-# mb
+read -e -p "What should the name of the S3 bucket be? The name of the bucket should take the form: s3://bucket-name with no uppercase letters and no spaces: " s3BucketName
 
-# sync into the bucket
+# Now $s3BucketName will work as the variable for the name of the S3 bucket
 
-# passthrough the info that the aws s3 cli would be displaying?
+# Let's make the S3 bucket first, because this is probably the most likely source of an error. According to Amazon, "An Amazon S3 bucket name is globally unique, and the namespace is shared by all AWS accounts. This means that after a bucket is created, the name of that bucket cannot be used by another AWS account in any AWS Region until the bucket is deleted. You should not depend on specific bucket naming conventions for availability or security verification purposes."
 
-# let user know that the sealed upload to s3 has completed
+aws s3 mb $s3BucketName &&
+
+# Let's cd into the source directory, and then execute mhl seal for the whole directory, with the xxHash algorithm, which is nice and fast
+#	N.B. mhl must be run from inside the source directory, so best practice is to cd in to the directory right within the shell script itself: https://stackoverflow.com/a/10566581/
+
+cd "$sourceLocalDirectory"
+
+mhl seal -t xxhash * &&
+# Let's sync the data from the local folder into the bucket
+
+aws s3 sync "$sourceLocalDirectory" $s3BucketName &&
+
+# Whatever the program displays will just get passed through into the script: https://unix.stackexchange.com/a/266111/
+
+# Let's let the user know that the data has been sealed and ingested.
+
+echo "The data from <$sourceLocalDirectory> has been sealed with xxHash checksums and and ingested into the S3 bucket named <$s3BucketName>."
